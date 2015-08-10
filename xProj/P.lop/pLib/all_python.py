00690f9332dd02595459da5e780afc8465dbec11
yho <yho@ncsu.edu>
2015-08-10 03:51:37 -0400

    Renamed xLib to pLib and ran benchmarking tests

--- xProj/P.lop/pLib/all_python.py
+++ xProj/P.lop/pLib/all_python.py
@@ -9,29 +9,29 @@ thisFile = "all_python"
 
 workDir = os.path.dirname(os.path.realpath(__file__))
 thisDir = os.path.dirname(os.path.realpath(__file__))
-sys.path.extend([os.path.dirname(workDir) + "/xLib"])
+sys.path.extend([os.path.dirname(workDir) + "/pLib"])
 
 print ".. importing files from this SANDBOX directory \n{}".format(thisDir)
 
-thisDir = "../../../xBed/xLib"
+thisDir = "../../../xBed/pLib"
 os.chdir(thisDir)
 thisDir = os.getcwd()
 sys.path.extend([thisDir])
 
-print ".. importing files from xBed/xLib directory \n{}".format(thisDir)
+print ".. importing files from xBed/pLib directory \n{}".format(thisDir)
 
 os.chdir(workDir)
 print ".. returning to directory \n{}".format(workDir)
 print time.strftime("%a %b %d %H:%M:%S %Z %Y")
 
 from config import *
-#sys.path.extend(["./xLib","../../../xBed/xLib"])
+#sys.path.extend(["./pLib","../../../xBed/xLib"])
 
 thisDir = workDir
 sandboxPath = os.path.dirname(thisDir) 
 sandboxName = os.path.basename(sandboxPath)
 infoVariablesFile = sandboxName + ".info_variables.txt"
-infoVariablesFile = "/".join([sandboxPath,"xLib",infoVariablesFile])
+infoVariablesFile = "/".join([sandboxPath,"pLib",infoVariablesFile])
 
 if os.path.isfile(infoVariablesFile):
     all_info["sandboxName"] = sandboxName
